4a5e358edb23d3c5e9f5e9290e923cc8ff1f6fb9
galtso <galcho_ta@yahoo.com>
2022-06-29 18:22:56 +0300

    Traffic Lights

---
+++ Working with Abstraction - Lab/TrafficLights/Main.java
@@ -0,0 +1,27 @@
+package TrafficLights;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] colors = scan.nextLine().split("\\s+");
+        int n = Integer.parseInt(scan.nextLine());
+
+        List<Light> trafficLight = new ArrayList<>();
+        for (String color : colors) {
+            Light light = new Light(Color.valueOf(color));
+            trafficLight.add(light);
+        }
+
+        for (int i = 0; i < n; i++) {
+            trafficLight.forEach(light -> {
+                light.changeColor();
+                System.out.print(light.getColor()+ " ");
+            });
+            System.out.println();
+        }
+    }
+}

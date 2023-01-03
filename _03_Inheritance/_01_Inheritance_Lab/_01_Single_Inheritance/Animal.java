9e83b1e4c19b9b3d37c1d16a6136329194816ff7
galtso <galcho_ta@yahoo.com>
2023-01-03 20:03:58 +0200

    Added _01_Single_Inheritance

---
+++ _03_Inheritance/_01_Inheritance_Lab/_01_Single_Inheritance/Animal.java
@@ -0,0 +1,7 @@
+package _03_Inheritance._01_Inheritance_Lab._01_Single_Inheritance;
+
+public class Animal {
+    public void eat(){
+        System.out.println("eating...");
+    }
+}
---
+++ _03_Inheritance/_01_Inheritance_Lab/_01_Single_Inheritance/Main.java
@@ -0,0 +1,9 @@
+package _03_Inheritance._01_Inheritance_Lab._01_Single_Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}

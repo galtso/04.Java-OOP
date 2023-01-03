f8fb95e5b7bac30ed35433d701c6b1c4906b9a45
galtso <galcho_ta@yahoo.com>
2023-01-03 21:01:33 +0200

    Added _01_Person

---
+++ _03_Inheritance/_02_Inheritance-Exercises/_01_Person/Child.java
@@ -0,0 +1,7 @@
+package _03_Inheritance._02_Inheritance_Exercises._01_Person;
+
+public class Child extends Person{
+    public Child(String name, int age) {
+        super(name, age);
+    }
+}
---
+++ _03_Inheritance/_02_Inheritance-Exercises/_01_Person/Main.java
@@ -0,0 +1,7 @@
+package _03_Inheritance._02_Inheritance_Exercises._01_Person;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}

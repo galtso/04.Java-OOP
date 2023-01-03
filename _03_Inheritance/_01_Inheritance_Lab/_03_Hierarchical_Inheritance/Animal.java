55b62a423d0e455e2492207946d53440c3522d4c
galtso <galcho_ta@yahoo.com>
2023-01-03 20:11:16 +0200

    Added _03_Hierarchical_Inheritance

---
+++ _03_Inheritance/_01_Inheritance_Lab/_03_Hierarchical_Inheritance/Animal.java
@@ -0,0 +1,7 @@
+package _03_Inheritance._01_Inheritance_Lab._03_Hierarchical_Inheritance;
+
+public class Animal {
+    public void eat(){
+        System.out.println("eating...");
+    }
+}
---
+++ _03_Inheritance/_01_Inheritance_Lab/_03_Hierarchical_Inheritance/Main.java
@@ -0,0 +1,8 @@
+package _03_Inheritance._01_Inheritance_Lab._03_Hierarchical_Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}

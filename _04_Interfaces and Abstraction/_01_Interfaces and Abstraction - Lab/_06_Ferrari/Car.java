f3aefefd7404b38f5cd1657a9fb12a274a13355d
galtso <galcho_ta@yahoo.com>
2023-01-06 18:11:03 +0200

    Added _06_Ferrari

---
+++ _04_Interfaces and Abstraction/_01_Interfaces and Abstraction - Lab/_06_Ferrari/Car.java
@@ -0,0 +1,6 @@
+package _04_Interfaces_And_Abstraction._01_Interfaces_And_Abstraction_Lab._06_Ferrari;
+
+public interface Car {
+    String brakes();
+    String gas();
+}
---
+++ _04_Interfaces and Abstraction/_01_Interfaces and Abstraction - Lab/_06_Ferrari/Main.java
@@ -0,0 +1,7 @@
+package _04_Interfaces_And_Abstraction._01_Interfaces_And_Abstraction_Lab._06_Ferrari;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}

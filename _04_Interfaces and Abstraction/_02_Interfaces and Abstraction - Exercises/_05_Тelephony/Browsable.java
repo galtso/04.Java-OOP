db2850d6d674f94508a88b810779ea021ab8e6bd
galtso <galcho_ta@yahoo.com>
2023-01-07 16:40:57 +0200

    Added _05_Тelephony

---
+++ _04_Interfaces and Abstraction/_02_Interfaces and Abstraction - Exercises/_05_Тelephony/Browsable.java
@@ -0,0 +1,5 @@
+package _04_Interfaces_And_Abstraction._02_Interfaces_And_Abstraction_Exercises._05_Тelephony;
+
+public interface Browsable {
+    String browse();
+}
---
+++ _04_Interfaces and Abstraction/_02_Interfaces and Abstraction - Exercises/_05_Тelephony/Main.java
@@ -0,0 +1,7 @@
+package _04_Interfaces_And_Abstraction._02_Interfaces_And_Abstraction_Exercises._05_Тelephony;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
